fe7feb8fe71885daef2cba5bb339b543e327fcc1
Giovanny Moreno <44243879+Giovannymo@users.noreply.github.com>
2023-07-13 14:00:23 -0500

    Fetch meals and template html

--- js/script.js
+++ js/script.js
@@ -2,15 +2,45 @@ import { getData } from "./data.js";
 
 const URL = "https://www.themealdb.com/api/json/v1/1/";
 const CATEGORIES = "categories.php";
+const FILTER_CATEGORY = "filter.php?c=";
 
 const $containerCards = document.getElementById("container-cards");
 
 
 (async ()=>{
     const data = await getData(URL+CATEGORIES)
+<<<<<<< HEAD
     drawCategories(data)
 })();    
+=======
+    showCategories(data)
+})();
+>>>>>>> 2a2c552 (Fetch meals and template html)
 
+async function instructionsMeal(id){
+    const response = await fetch("https://www.themealdb.com/api/json/v1/1/lookup.php?i="+id);
+    const data = await response.json();
+    return data
+}
+
+function showMeals(data){
+    const $containerMeals = document.getElementById("container-cards-meals")
+    const $template = document.getElementById("card-meals").content;
+    const $fragment = document.createDocumentFragment();
+
+    
+    data.forEach(async (meal) =>{
+        const $nameRecipe = $template.querySelector("card-title");
+        $nameRecipe.textContent = meal.strMeal;
+
+        const $imgMeal = $template.querySelector(".card-img-top");
+
+        $imgMeal.src = meal.strMealThumb;
+
+        const instructions = await instructionsMeal(meal.idMeal);
+
+    })
+}
 
 $containerCards.addEventListener("click", (e)=>{
 
@@ -21,8 +51,16 @@ $containerCards.addEventListener("click", (e)=>{
 })
 
 
-function drawCategories(data){
+
+
+
+
+function showCategories(data){
     console.log(data)
+<<<<<<< HEAD
+=======
+    const $containerCards = document.getElementById("container-cards-categories");
+>>>>>>> 2a2c552 (Fetch meals and template html)
     const $fragment = document.createDocumentFragment();
     const $template = document.getElementById("card-category").content;
 

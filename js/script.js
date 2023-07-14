33692914ff2d6f40a6b5bfb57f83a6f265356a21
Giovanny Moreno <giovannysocas@gmail.com>
2023-07-14 00:40:35 -0500

    add:  show meals from categories

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const URL = "https://www.themealdb.com/api/json/v1/1/";
 const CATEGORIES = "categories.php";
 const FILTER_CATEGORY = "filter.php?c=";
 
-const $containerCards = document.getElementById("container-cards");
+const $containerCards = document.getElementById("container-cards-categories");
 
 
 (async ()=>{
@@ -13,40 +13,45 @@ const $containerCards = document.getElementById("container-cards");
 })();
 
 
-async function instructionsMeal(id){
-    const response = await fetch("https://www.themealdb.com/api/json/v1/1/lookup.php?i="+id);
-    const data = await response.json();
-    return data
-}
-
-function showMeals(data){
-    const $containerMeals = document.getElementById("container-cards-meals")
-    const $template = document.getElementById("card-meals").content;
-    const $fragment = document.createDocumentFragment();
-
-    
-    data.forEach(async (meal) =>{
-        const $nameRecipe = $template.querySelector("card-title");
-        $nameRecipe.textContent = meal.strMeal;
-
-        const $imgMeal = $template.querySelector(".card-img-top");
-
-        $imgMeal.src = meal.strMealThumb;
-
-        const instructions = await instructionsMeal(meal.idMeal);
-
-    })
-}
 
 $containerCards.addEventListener("click", (e)=>{
 
     if(e.target.classList.contains("card-link")){
-
+        const $container = e.target.parentNode.parentNode
+        const $title = $container.querySelector(".card-title")
+        showMeals($title.textContent)
     }
    
 })
 
 
+async function showMeals(category){
+    const $containerMeals = document.getElementById("container-cards-meals");
+    $containerMeals.innerHTML = ""
+    const $template = document.getElementById("card-meals").content;
+    const $fragment = document.createDocumentFragment();
+
+    const data = await getData(URL+FILTER_CATEGORY+category)
+    console.log(data);
+
+
+    data.meals.forEach((meal) =>{
+        console.log($containerMeals);
+        const $nameRecipe = $template.querySelector(".card-title");
+        $nameRecipe.textContent = meal.strMeal;
+
+        const $imgMeal = $template.querySelector(".card-img-top");  
+        $imgMeal.src = meal.strMealThumb;
+
+        const $category = document.querySelector(".name-category")
+        $category.textContent = category
+
+        const clone = $template.cloneNode(true);
+        $fragment.appendChild(clone)
+
+    })
+    $containerMeals.appendChild($fragment)
+}
 
 
 
